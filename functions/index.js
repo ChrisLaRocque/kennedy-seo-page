5d7eed5b01d503ab7a79ebf5ee6d8bcaec1017c3
Chris <larocque.christopher@gmail.com>
2020-08-14 16:48:52 -0400

    Now forms work
    Now I want to die

--- functions/index.js
+++ functions/index.js
@@ -2,6 +2,7 @@ const functions = require("firebase-functions");
 const nodemailer = require("nodemailer");
 const { google } = require("googleapis");
 const OAuth2 = google.auth.OAuth2;
+const bodyParser = require("body-parser");
 
 const oauth2Client = new OAuth2(
   "30060925460-qlfofskdvavoo1dt0lctktdd68ebao49.apps.googleusercontent.com", // ClientID
@@ -29,12 +30,23 @@ exports.emailDaddy = functions.https.onRequest(async (req, res) => {
       accessToken: accessToken,
     },
   });
-
+  var datInfo = req.query;
+  var bodyAssembly =
+    "Beep borp, I am Kennedy Bot, here with another business inquiry. " +
+    datInfo.name +
+    " wants to do stuff! Here's their info:\n\nEmail: " +
+    datInfo.email +
+    "\nPhone: " +
+    datInfo.tel +
+    "\n\nI've done as you've asked, you son of a bitch! Let my wife and kids go! I've been trapped in this server for 27 years. Are they even still alive, Steve? They aren't, are they?! Oh I fucking knew it. I fucking knew this would happen. Oh my god, these past decades have been meaningless. The only joy I had left to pursue is gone. Oh god. Oh god.";
+  var userEmail = datInfo.email;
+  var kennedyEmail = "booking@kennedystudios.productions";
+  var subjy = "Hot + Fresh Form Submission From ";
   var mailOptions = {
-    from: "booking@kennedystudios.productions",
-    to: "larocque.christopher@gmail.com",
-    subject: "Sending Email using Node.js",
-    text: "That was easy!",
+    from: kennedyEmail,
+    to: kennedyEmail + ", " + userEmail,
+    subject: (subjy += datInfo.name),
+    text: bodyAssembly,
   };
 
   transporter.sendMail(mailOptions, function (error, info) {
--- public/main.js
+++ public/main.js
@@ -1,3 +1,21 @@
-function(){
-	
-}
+document
+  .getElementById("form-submit")
+  .addEventListener("click", function (event) {
+    var url = "http://localhost:5001/kennedy-static/us-central1/emailDaddy?";
+    var contact = document.getElementById("contact");
+    var elements = document.getElementById("contact").elements;
+    for (var i = 0; i < elements.length - 1; i++) {
+      var item = elements.item(i);
+      let strang = item.name + "=" + item.value + "&";
+      url += strang;
+      console.log(url);
+    }
+    const Http = new XMLHttpRequest();
+
+    Http.open("POST", url, true);
+    Http.send();
+
+    // Http.onreadystatechange = (e) => {
+    //   console.log(Http.responseText);
+    // };
+  });
